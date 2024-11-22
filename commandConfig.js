78857a38cd1f4658f46eea566cd30c648dfd693d
wangfan <wangfan@frutech.com.cn>
2024-11-22 21:47:30 +0800

    update:1.41

--- commandConfig.js
+++ commandConfig.js
@@ -1,35 +1,34 @@
 module.exports = [
-    {
-        command: "api <url>",
-        alias: "a",
-        description: "根据swagger文档,在src/api目录下生成axios可用的js文件",
-        action: (option) => {
-            require('./src/modules/generateApiFile/index.js')(option)
-        }
+  {
+    command: "api <url>",
+    alias: "a",
+    description: "根据swagger文档,在src/api目录下生成axios可用的js文件",
+    action: (option) => {
+      require("./src/modules/generateApiFile/index.js")(option);
     },
-    {
-        command: "weather [city]",
-        alias: "w",
-        description: "查看天气预报",
-        action: (option) => {
-            require('./src/modules/weather/index.js')(option)
-        }
+  },
+  // {
+  //     command: "weather [city]",
+  //     alias: "w",
+  //     description: "查看天气预报",
+  //     action: (option) => {
+  //         require('./src/modules/weather/index.js')(option)
+  //     }
+  // },
+  {
+    command: "sethost [domain]",
+    alias: "sh",
+    description: "获取github 最新DNS，更新host",
+    action: (option) => {
+      require("./src/modules/sethost/index.js")(option);
     },
-    {
-        command: "sethost [domain]",
-        alias: "sh",
-        description: "获取github 最新DNS，更新host",
-        action: (option) => {
-            require('./src/modules/sethost/index.js')(option)
-        }
+  },
+  {
+    command: "create <name>",
+    alias: "c",
+    description: "创建项目",
+    action: (option) => {
+      require("./src/modules/create/index.js")(option);
     },
-    {
-        command: "create <name>",
-        alias: "c",
-        description: "创建项目",
-        action: (option) => {
-            require('./src/modules/create/index.js')(option)
-        }
-    },
-
-]
+  },
+];
